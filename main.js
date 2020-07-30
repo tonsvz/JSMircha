e30ceb231dc8df3b9311d8df20732d8979f62fc9
Tony Veliz <tonsvz@gmail.com>
2020-07-29 22:12:03 -0500

    Correciones pertinentes

--- main.js
+++ main.js
@@ -17,10 +17,35 @@ f.push("black") // agrega un elemento al final
 console.log(f);
 f.pop(); // elimina el ultimo elemento
 console.log(f);
-f.forEach((el, index) => {
+f.forEach((el, index) => { // metodo for.each
 console.log(`<li id="${index}">${el}</li>`); //iteraciones.
 })
 
-const tony = {pasatiempo: "jugar, programar", nombre: "Tony", apellido:"veliz", edad:30} //objeto
+
+//dentro de un objeto las variables se le conocen como propiedades y a las funciones se les conoce como metodos.
+const tony = {pasatiempo: "jugar, programar", nombre: "Tony", apellido:"veliz", edad:30, sayname:function () {
+    console.log(`Mi nombre es ${this.nombre}`);
+    
+}} //objeto
 console.log(tony);//objeto imprimido
 console.log(tony.edad);//forma de accesar a un atributo de objeto
+tony.sayname()
+console.log(Object.keys(tony)); // => Operadores
+console.log(Object.values(tony)); // => Valores
+console.log(tony.hasOwnProperty("nombre")); //Propiedad para evaluar valores dentro dele objeto
+console.log(tony.hasOwnProperty("nacimiento"));
+
+let a = 5 + (5-10) * 3;
+let modulo = 5%2;
+
+console.log(a);
+console.log(modulo);
+
+
+//operadores matematicos
+
+/* 
+
+= 1 igual es asignacion de variable
+== 2 iguales es comparacion de valores  <--- actualmente esto es una mala practica 
+=== 3 iguales es comparacion de tipo de dato y de valor <--- La buena practica te indica usar esto, esto es para verificar si es ESTRICTAMENTE igual al otro valor a comparar*/ 
