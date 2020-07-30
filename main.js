1d0257a17707136ddfd9e4d071e332af3186db37
Tony Veliz <tonsvz@gmail.com>
2020-07-29 22:36:11 -0500

    Correciones Pertinentes

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.write(fullName)
 console.log(nombre,apellido,saludo);
 console.log(c);
 console.log(nombre.length, apellido.length,saludo.length, nombre.toUpperCase(), lorem.includes("dolor"), lorem.trim(), lorem.split(" "));
-console.log(c.length);//midde longitud del objeto 
+console.log(c.length);//midde longitud del objeto
 console.log(d);
 
 const f = new Array(1,2,3,4,true);//Antigua y obsoleta manera de declarar arrays.
@@ -25,7 +25,7 @@ console.log(`<li id="${index}">${el}</li>`); //iteraciones.
 //dentro de un objeto las variables se le conocen como propiedades y a las funciones se les conoce como metodos.
 const tony = {pasatiempo: "jugar, programar", nombre: "Tony", apellido:"veliz", edad:30, sayname:function () {
     console.log(`Mi nombre es ${this.nombre}`);
-    
+
 }} //objeto
 console.log(tony);//objeto imprimido
 console.log(tony.edad);//forma de accesar a un atributo de objeto
@@ -35,6 +35,15 @@ console.log(Object.values(tony)); // => Valores
 console.log(tony.hasOwnProperty("nombre")); //Propiedad para evaluar valores dentro dele objeto
 console.log(tony.hasOwnProperty("nacimiento"));
 
+
+//operadores matematicos
+
+/*
+
+= 1 igual es asignacion de variable
+== 2 iguales es comparacion de valores  <--- actualmente esto es una mala practica
+=== 3 iguales es comparacion de tipo de dato y de valor <--- La buena practica te indica usar esto, esto es para verificar si es ESTRICTAMENTE igual al otro valor a comparar*/
+
 let a = 5 + (5-10) * 3;
 let modulo = 5%2;
 
@@ -42,10 +51,35 @@ console.log(a);
 console.log(modulo);
 
 
-//operadores matematicos
+console.log(7 == 7);
+console.log("7" == 7);
+console.log(0 == false);
 
-/* 
+console.log(7 === 7);
+console.log("7" === 7);
+console.log(0 === false);
 
-= 1 igual es asignacion de variable
-== 2 iguales es comparacion de valores  <--- actualmente esto es una mala practica 
-=== 3 iguales es comparacion de tipo de dato y de valor <--- La buena practica te indica usar esto, esto es para verificar si es ESTRICTAMENTE igual al otro valor a comparar*/ 
+/*Operadores de incremento y decremento*/
+
+let i = 2;
+
+// i = i +3; // es lo mismo
+// i+=3; // forma correcta de declarar incrementos.
+
+//operador unario;
+i++; //incrementa el valor de 1 en 1.
+console.log(i);
+
+
+//Operadores logicos
+/*
+!-Not   Negacion, lo que es true lo convierte en false y visceversa
+||- Or <-- alt gr +1 Cuando se tengan 2 o mas condiciones con que una se cumpla el OR podra validar.
+&& - And */  //Cuando se tengan 2 o mas condiciones todas tienen que cumplirse para que el AND pueda validar, es decir ser verdaderas
+console.log((9===9) || ("9"=== 9)); //OR
+
+console.log((9===9) && (9===9)); //AND
+
+
+
+
