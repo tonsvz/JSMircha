6d6020c72bc9feeb217891a899626c8414ed30d1
Tony Veliz <tonsvz@gmail.com>
2020-12-05 14:36:26 -0500

    sabado

--- mainEjercicios.js
+++ mainEjercicios.js
@@ -13,8 +13,7 @@ button2.addEventListener("click", () => {
     alert("Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion (Hola Mundo, 4) devolverá: Hola")
     let str = prompt("Inserte un texto a evaluar");
     let cropText = prompt("Ingrese cantidad de caracteres a recortar")
-    ex02(str, cropText)
-
+    ex02(str, cropText)        
 })
 button3.addEventListener("click", () => {
     alert("Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']")
